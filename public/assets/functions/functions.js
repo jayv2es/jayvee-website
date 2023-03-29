ccbf5897036f1621712803ea714e2a15308ba6f0
Jay Vee <joel98@hispeed.ch>
2023-03-29 20:54:02 +0200

    Syntax mistake

--- public/assets/functions/functions.js
+++ public/assets/functions/functions.js
@@ -907,6 +907,7 @@ function changeSubmenu(oldId, newId, indexFlag, reverseDirFlag = false) {
               // -------------------------------------------
             },
           }
+        )
       },
     }
   );
